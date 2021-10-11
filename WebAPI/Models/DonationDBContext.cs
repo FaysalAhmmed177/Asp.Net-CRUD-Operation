75c6eb9e6f2a331e3ce04208947af2b26849bc8b
Fayal Ahmmed <faysalahmmed177@gmail.com>
2021-10-11 09:40:45 +0600

    Add project files.

---
+++ WebAPI/Models/DonationDBContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Models
+{
+    public class DonationDBContext : DbContext
+    {
+        public DonationDBContext(DbContextOptions<DonationDBContext> options) : base(options)
+        {
+
+        }
+        public DbSet<DCandidate> DCandidates { get; set; }
+    }
+}
